d56cedd146df16a75be31767c0f893ab3215c190
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-10-09 21:32:03 +0530

    Initial Commit

---
+++ Carousel List SwiftUI/Carousel_List_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_List_SwiftUIApp.swift
+//  Carousel List SwiftUI
+//
+//  Created by Mahesh Prasad on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_List_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel List SwiftUITests/Carousel_List_SwiftUITests.swift
@@ -0,0 +1,33 @@
+//
+//  Carousel_List_SwiftUITests.swift
+//  Carousel List SwiftUITests
+//
+//  Created by Mahesh Prasad on 09/10/20.
+//
+
+import XCTest
+@testable import Carousel_List_SwiftUI
+
+class Carousel_List_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
